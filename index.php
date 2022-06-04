2003d3791cd7c9eae39aa83bf0deb7c8a8e27ae5
MD NABIL HOSSAIN <49895745+mdnabil9@users.noreply.github.com>
2022-06-05 01:25:26 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!-- Navigation Bar -->
+	<?php include 'navbar.php'?>
+<!-- Navigation Bar -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>BdDiary</title>
+	<meta name="google-site-verification" content="ZO56SO-iUzj5Zbv4jgqotjX9tEkli9ngLLFyHp_faFo" />
+	<!-- Viewport for Different Devices -->
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Viewport for Different Devices -->
+
+	<!-- Logo of the Title -->
+		<link rel="icon" href="images/bddiary.jpg">
+	<!-- Logo of the Title -->
+
+	<!-- BootStrap Stylesheet LInk -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<!-- BootStrap Stylesheet LInk -->
+</head>
+
+<body>
+	<!-- Main Body -->
+	<div class="container text-center">
+		<h3>Download Android App</h3>
+		<a href="https://appsgeyser.io/15225407/BD-Diary" target="blank" class="btn btn-primary">Download</a><br>
+		<img src="images/apk_qr_code.jpg" alt="App Download QR Code not found!!">
+	</div>
+	<div class="container">
+		<!-- <table>
+		  <tr onclick="window.open('tel:01683316110');" style="cursor: pointer;">
+		    <td>Dial me 016 ..</td>
+		  </tr>
+		</table> -->
+		<!-- <button class="glyphicon glyphicon-phone-alt btn btn-success" onclick="window.open('tel:01683316110');"> 01683316110</button> -->
+		<!-- <button class="glyphicon glyphicon-phone-alt btn btn-success" onclick="window.open('tel:01683316110', '_self');"> 01683316110</button> -->
+	</div>
+	<!-- Main Body -->
+	<!-- BootStrap Javascript LInk -->
+		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+	    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+	    
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<!-- BootStrap Javascript LInk -->
+	<footer style="text-align: center; font-size: 16px;" class="border pt-1 pb-1 m-0 hidden-print">Copyright &copy; 2022 <b>MD NABIL HOSSAIN</b> <i>All Rights Reserved</i></footer>
+</body>
+</html>
